8773588dd2705d26402b986baef2b73e40f7e542
eric luis <ericluismauricio@gmail.com>
2025-04-16 15:51:50 -0300

    add home screen page

--- frontend/RXLog/src/components/header/header.jsx
+++ frontend/RXLog/src/components/header/header.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../header/header.module.css";
+import { FaBars, FaUserCircle } from "react-icons/fa";
 
-function Header(){
-    
-}
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+
+        <div
+          className={styles.menuButton}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </div>
+
+        <div className={styles.logoContainer}>
+          <h1 className={styles.logo}>RxLog</h1>
+        </div>
+      </div>
+
+        <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ""}`}>
+          <a href="#home">Home</a>
+          <a href="#dashboard">DashBoards</a>
+          <a href="#contato">Contato</a>
+          <a href="#gestao">Gestão</a>
+        </nav>
+
+        <button className={styles.userButton}>
+          <FaUserCircle className={styles.userImage}/>
+        </button>
+    </header>
+  );
+};
 
 export default Header;
