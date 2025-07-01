db3a0bac09731b12e0176a02d1469a20b2f7c3f0
eric luis <ericluismauricio@gmail.com>
2025-06-30 23:12:49 -0300

    feat: add maturity cards

--- frontend/RXLog/src/pages/Home/homeScreen.jsx
+++ frontend/RXLog/src/pages/Home/homeScreen.jsx
@@ -1,10 +1,34 @@
+import React, { useEffect } from "react";
 import Header from "../../components/header/header";
 import Carrossel from "../../components/carrossel/carrossel";
 import Footer from "../../components/footer/footer";
 import styles from "./homeScreen.module.css";
-import image from "../../assets/images/image.png"
+import image from "../../assets/images/image.png";
+
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function HomeScreen() {
+  useEffect(() => {
+    axios.get("http://localhost:8080/alertas/vencidos")
+      .then((res) => {
+        res.data.forEach((med) => {
+          toast.warn(`Medicamento vencido: ${med.nomeComercial} (Lote: ${med.loteMedicamento})`, {
+            autoClose: 5000,
+            hideProgressBar: false,
+            position: "top-right",
+            pauseOnHover: true,
+            closeOnClick: true,
+            draggable: true
+          });
+        });
+      })
+      .catch((err) => {
+        console.error("Erro ao buscar medicamentos vencidos", err);
+      });
+  }, []);
+
   return (
     <>
       <Header />
@@ -12,7 +36,7 @@ function HomeScreen() {
       <section className={styles.aboutSection}>
         <div className={styles.aboutContent}>
           <div className={styles.aboutImage}>
-            <img src={image} alt="Ilustracao RxLog" />
+            <img src={image} alt="Ilustração RxLog" />
           </div>
           <div className={styles.aboutText}>
             <h2>QUEM SOMOS</h2>
@@ -21,7 +45,7 @@ function HomeScreen() {
               visa a tecnologia e a facilidade dentro da área da saúde. Fundada
               em 2025, a RxLog trouxe uma nova abordagem ao ecossistema de
               armazenamento de medicamentos nos hospitais públicos, que muitas
-              das vezes não possuiam sistemas de armazenamento. Nosso
+              das vezes não possuíam sistemas de armazenamento. Nosso
               compromisso é acelerar mudanças e promover a inovação, sempre
               focados nas transformações do mundo e na construção de soluções
               que facilitem a vida cotidiana.
@@ -30,6 +54,7 @@ function HomeScreen() {
         </div>
       </section>
       <Footer />
+      <ToastContainer />
     </>
   );
 }
