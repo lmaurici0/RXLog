3fc32c801ac4c79c55648b2976959b0d4c74d5a3
eric luis <ericluismauricio@gmail.com>
2025-07-01 19:14:55 -0300

    feat: add new dashboards

--- frontend/RXLog/src/pages/DashBoard/DashBoard.jsx
+++ frontend/RXLog/src/pages/DashBoard/DashBoard.jsx
@@ -1,14 +1,58 @@
-import Graphic from "../../components/graphic/graphic";
+import {React, useEffect} from "react";
+
 import Header from "../../components/header/header";
-import Button from "../../components/button/button";
+import styles from "../DashBoard/DashBoard.module.css";
+
+import EstoqueChart from "../../components/layout/Dashboard/EstoqueChart/EstoqueChart";
+import EntradasChart from "../../components/layout/Dashboard/EntradasChart/EntradasChart";
+import SaidasChart from "../../components/layout/Dashboard/SaidasChart/SaidasChar";
+import Footer from "../../components/footer/footer";
+import TabelasMedicamentos from "../../components/layout/Dashboard/Table/Table";
+import PizzaChart from "../../components/layout/Dashboard/PizzaChart/PizzaChart";
+
 
 function DashBoard() {
+  const usuario = {
+    nome: "Eric Luis",
+    email: "ericluismauricio@gmail.com",
+    cargo: "Farmacêutico Responsável",
+  };
+
+  const movimentacoes = {
+    entradas: 535,
+    saidas: 290,
+  };
+
   return (
     <>
       <Header />
-      <Graphic />
+      <div className={styles.wrapper}>
+        <div className={styles.barChart}>
+          <EstoqueChart />
+          <PizzaChart />
+        </div>
+
+        <div className={styles.lineChart}>
+          <EntradasChart />
+          <SaidasChart />
+        </div>
+
+        <div className={styles.tables}>
+          <TabelasMedicamentos />
+        </div>
+
+        <div className={styles.cards}>
+          <div className={styles.movCard}>
+            <h2>Minhas Movimentações</h2>
+            <p><strong>Entradas:</strong> {movimentacoes.entradas}</p>
+            <p><strong>Saídas:</strong> {movimentacoes.saidas}</p>
+          </div>
+        </div>
+
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default DashBoard;
