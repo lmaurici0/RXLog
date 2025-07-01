e3f4fc6e36382cb85c642cb93ce7747931f2065a
eric luis <ericluismauricio@gmail.com>
2025-06-30 22:03:45 -0300

    style: remake footer

--- frontend/RXLog/src/components/footer/footer.jsx
+++ frontend/RXLog/src/components/footer/footer.jsx
@@ -1,39 +1,67 @@
-import React from 'react';
-import styles from './Footer.module.css';
+import React from "react";
+import styles from "./Footer.module.css";
 import { AiTwotoneMail } from "react-icons/ai";
 import { CiInstagram, CiLinkedin } from "react-icons/ci";
-import { IoLocationOutline } from "react-icons/io5";
+import { MdLocationOn } from "react-icons/md";
+
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.contactContent}>
-        <h2>Contato</h2>
+        <h2>Fale Conosco</h2>
+
         <div className={styles.social}>
           <div className={styles.contactItem}>
             <AiTwotoneMail />
             <a href="mailto:somosrx@gmail.com">somosrx@gmail.com</a>
           </div>
-          
           <div className={styles.contactItem}>
             <CiInstagram />
-            <a href="#instagram">@somosrx</a>
+            <a
+              href="https://instagram.com/somosrx"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @somosrx
+            </a>
           </div>
-
           <div className={styles.contactItem}>
             <CiLinkedin />
-            <a href="#linkedin">/somosrx</a>
+            <a
+              href="https://linkedin.com/company/somosrx"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              /somosrx
+            </a>
           </div>
-
           <div className={styles.contactItem}>
-          <IoLocationOutline />
-            <span>Rua Alcantara, 113. Vila Maria Baixa – São Paulo</span>
+       <MdLocationOn style={{ fontSize: "50px" }} />
+            <a
+              href="https://linkedin.com/company/somosrx"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.locationIcon}
+            >
+              Rua Alcantara, 113. Vila Maria Baixa. São Paulo
+            </a>
           </div>
         </div>
 
+        <hr
+          style={{
+            width: "100%",
+            maxWidth: 600,
+            border: "0.5px solid #333",
+            margin: "30px 0",
+          }}
+        />
 
         <div className={styles.logoContainer}>
-          <h3 className={styles.logo}>RxLog</h3>
+          <small>
+            © {new Date().getFullYear()} - Todos os direitos reservados
+          </small>
         </div>
       </div>
     </footer>
