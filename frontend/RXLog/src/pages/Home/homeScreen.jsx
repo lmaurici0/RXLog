bbeb4dd4d3059e77057a2b6231dacfa1e17d6828
eric luis <ericluismauricio@gmail.com>
2025-07-03 23:57:35 -0300

    fix: delete maturity card

--- frontend/RXLog/src/pages/Home/homeScreen.jsx
+++ frontend/RXLog/src/pages/Home/homeScreen.jsx
@@ -1,34 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "../../components/header/header";
 import Carrossel from "../../components/carrossel/carrossel";
 import Footer from "../../components/footer/footer";
 import styles from "./homeScreen.module.css";
 import image from "../../assets/images/image.png";
 
-import axios from "axios";
-import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
 function HomeScreen() {
-  useEffect(() => {
-    axios.get("http://localhost:8080/alertas/vencidos")
-      .then((res) => {
-        res.data.forEach((med) => {
-          toast.warn(`Medicamento vencido: ${med.nomeComercial} (Lote: ${med.loteMedicamento})`, {
-            autoClose: 5000,
-            hideProgressBar: false,
-            position: "top-right",
-            pauseOnHover: true,
-            closeOnClick: true,
-            draggable: true
-          });
-        });
-      })
-      .catch((err) => {
-        console.error("Erro ao buscar medicamentos vencidos", err);
-      });
-  }, []);
-
   return (
     <>
       <Header />
