fd3f6896558fec07577b0210cc47a1fcbb1074ac
eric luis <ericluismauricio@gmail.com>
2025-09-05 18:45:51 -0300

    feat: create private route

--- frontend/RXLog/src/App.jsx
+++ frontend/RXLog/src/App.jsx
@@ -1,43 +1,35 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomeScreen from './pages/Home/homeScreen';
-import DashBoard from './pages/DashBoard/DashBoard';  
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from './pages/AuthPage/AuthPage';
-import Contact from './pages/Contact/Contact';
-import PageNotFound from './pages/Error/404/PageNotFound';
-import Forbidden from './pages/Error/403/Forbidden';
-import Unauthorized from './pages/Error/401/Unauthorized';
-import Profile from './pages/Profile/Profile';
+import DashBoard from './pages/DashBoard/DashBoard';
 import MedicamentRegistration from './pages/MedicamentRegister/MedicamentRegistration';
 import MedicamentExit from './pages/MedicamentExit/MedicamentExit';
+import HomeScreen from './pages/Home/homeScreen';
+import Contact from './pages/Contact/Contact';
+import Profile from './pages/Profile/Profile';
+import Unauthorized from './pages/Error/401/Unauthorized';
+import Forbidden from './pages/Error/403/Forbidden';
+import PageNotFound from './pages/Error/404/PageNotFound';
 
-const handleApiError = (status) => {
-    switch (status) {
-      case 401:
-        return <Navigate to="/unauthorized" />;
-      case 403:
-        return <Navigate to="/forbidden" />;
-      default:
-        return <Navigate to="/notfound" />;
-    }
-  };
-  
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/unauthorized" />;
+}
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AuthPage />} />
-        <Route path="/dashboards" element={<DashBoard />} /> 
-        <Route path='/sobre' element={<HomeScreen />} />
-        <Route path='/contato' element={<Contact />} />
-        <Route path='/perfil' element={<Profile />} />
-        <Route path='/medicamentos/cadastro' element={<MedicamentRegistration />} />
-        <Route path='/medicamentos/saida' element={<MedicamentExit />} />
-        
-        <Route path='/error/401' element={<Unauthorized />} />
-        <Route path='/error/403' element={<Forbidden />} />
-        <Route path='/error/404' element={<PageNotFound />} />
-        <Route path='*' element={<PageNotFound />} />
+        <Route path="/dashboards" element={<PrivateRoute><DashBoard /></PrivateRoute>} />
+        <Route path="/medicamentos/cadastro" element={<PrivateRoute><MedicamentRegistration /></PrivateRoute>} />
+        <Route path="/medicamentos/saida" element={<PrivateRoute><MedicamentExit /></PrivateRoute>} />
+        <Route path="/sobre" element={<HomeScreen />} />
+        <Route path="/contato" element={<Contact />} />
+        <Route path="/perfil" element={<Profile />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+        <Route path="/error/403" element={<Forbidden />} />
+        <Route path="/error/404" element={<PageNotFound />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
   );
