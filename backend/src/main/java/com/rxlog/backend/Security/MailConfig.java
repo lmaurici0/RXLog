19b679c86f0ae8646be5608f70f42c780eb19b2c
eric luis <ericluismauricio@gmail.com>
2025-09-18 00:03:19 -0300

    fix: fix email

--- backend/src/main/java/com/rxlog/backend/Security/MailConfig.java
+++ backend/src/main/java/com/rxlog/backend/Security/MailConfig.java
@@ -14,8 +14,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("rxlogenterprise@gmail.com");
-        mailSender.setPassword("euls nckk mfqm htpt\n");
+        mailSender.setUsername("{YOUR EMAIL}");
+        mailSender.setPassword("{YOUR PASS}\n");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
