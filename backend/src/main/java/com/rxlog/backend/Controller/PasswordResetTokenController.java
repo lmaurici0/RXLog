a592a5a360e2397b1e08b940464c2dbd3daadfb0
eric luis <ericluismauricio@gmail.com>
2025-09-18 00:02:23 -0300

    fix: change import

--- backend/src/main/java/com/rxlog/backend/Controller/PasswordResetTokenController.java
+++ backend/src/main/java/com/rxlog/backend/Controller/PasswordResetTokenController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
